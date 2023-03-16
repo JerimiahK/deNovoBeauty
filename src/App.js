abe3dc94bea2adfd1bb819c364a673778c751045
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-03-16 11:57:36 -0600

    set up routing in App.js, as well as made files for pages and the navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import About from "./pages/About";
+import Services from "./pages/Connect";
+import Connect from "./pages/Connect";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/connect" element={<Connect />} />
+      </Routes>
+    </>
   );
 }
 
