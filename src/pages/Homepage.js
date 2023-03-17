2d04311b61cd3323d4c869ff7ef9fe73169e1c01
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-03-17 17:54:03 -0600

    finished beta version of the homepage carousel. Added a footer to the page

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,7 @@
 import HomeHeader from "../components/Organisms/HomePage/Header/Header";
 import Testimonials from "../components/Organisms/HomePage/Testimonials/Testimonials";
 import NavBar from "../components/Molecules/NavBar/NavBar";
+import Footer from "../components/Molecules/Footer/Footer";
 
 export default function Homepage() {
   return (
@@ -8,6 +9,7 @@ export default function Homepage() {
       <HomeHeader />
       <NavBar />
       <Testimonials />
+      <Footer />
     </>
   );
 }
