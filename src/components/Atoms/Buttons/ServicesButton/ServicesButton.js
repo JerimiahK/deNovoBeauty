a1cadf2e809e58b7bbae7ed51e5f12da0ae1fbd7
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-03-17 21:01:53 -0600

    added a button to the services section of the homepage

---
+++ src/components/Atoms/Buttons/ServicesButton/ServicesButton.js
@@ -0,0 +1,11 @@
+import "./servicesButton.css";
+
+export default function ServicesButton() {
+    return (
+        <div className="buttonContainer">
+            <a href="/services">
+                <button className="button">View Services!</button>
+            </a>
+        </div>
+    );
+}
