4d9f633b220356825ee223cf04790bad23130f9f
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-03-18 13:05:41 -0600

    styled the icon and social links in the footer

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,6 @@
 import HomeHeader from "../components/Organisms/HomePage/Header/Header";
-import Testimonials from "../components/Organisms/HomePage/Testimonials/Testimonials";
-import Services from "../components/Organisms/HomePage/Servicess/Services";
+import Testimonials from "../components/Organisms/HomePage/TestimonialSection/Testimonials";
+import Services from "../components/Organisms/HomePage/ServicesSection/Services";
 import NavBar from "../components/Molecules/NavBar/NavBar";
 import Footer from "../components/Organisms/HomePage/Footer/Footer";
 
