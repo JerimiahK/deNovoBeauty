1838bf86bd7c4bc7902509f733dc93cf2d9b30b5
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-03-19 15:42:56 -0600

    added the example photos to the bottom of the page

--- src/components/Molecules/AboutSection/AboutSection.js
+++ src/components/Molecules/AboutSection/AboutSection.js
@@ -1,12 +1,16 @@
 import "./aboutSection.css";
+import Headshot from "../../Atoms/Pictures/AboutPhotos/Headshot";
 import AboutText from "../../Atoms/Text/AboutPage/AboutText";
 import PhotoContainer from "./PhotoContainer";
 
 export default function AboutSection() {
     return (
-        <div className="aboutSection">
-            <AboutText />
+        <>
+            <div className="aboutSection">
+                <AboutText />
+                <Headshot />
+            </div>
             <PhotoContainer />
-        </div>
+        </>
     );
 }
--- src/components/Molecules/AboutSection/PhotoContainer.js
+++ src/components/Molecules/AboutSection/PhotoContainer.js
@@ -1,14 +1,12 @@
+import "./photoContainer.css";
 import AboutImg1 from "../../Atoms/Pictures/AboutPhotos/AboutImg1";
 import AboutImg2 from "../../Atoms/Pictures/AboutPhotos/AboutImg2";
-import Headshot from "../../Atoms/Pictures/AboutPhotos/Headshot";
-
 
 export default function PhotoContainer() {
     return (
-    <div className="photoContainer">
-        <Headshot />
-        <AboutImg1 />
-        <AboutImg2 />
-    </div>
+        <div className="photoContainer">
+            <AboutImg1 />
+            <AboutImg2 />
+        </div>
     );
 }
