048f9d196a6e85d569da8e267af1bd26f51bf0fa
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-03-20 17:47:52 -0600

    Finished the Bridal Services box.

---
+++ src/components/Atoms/ServiceCards/PersonalMakeup.js
@@ -0,0 +1,7 @@
+import "./personalMakeup.css";
+
+export default function PersonalMakeupCard() {
+  return (
+    <div>PersonalMakeupCard</div>
+  )
+}
--- src/components/Molecules/ServicesSection/ServicesContainer.js
+++ src/components/Molecules/ServicesSection/ServicesContainer.js
@@ -1,7 +1,9 @@
-import "./servicesSection.css";
+import "./servicesContainer.css";
+import BridalMakeupCard from "../../Atoms/ServiceCards/BridalMakeup";
 
 export default function ServicesSection() {
     return <div className="servicesSectionContainer">
         <h1 className="servicesHeaderText">Services</h1>
+        <BridalMakeupCard />
     </div>;
 }
