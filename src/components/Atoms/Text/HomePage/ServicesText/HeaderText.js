b067f40cc9c0bc4b1b4c28032ea3b3562b8cc304
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-03-21 17:37:38 -0600

    Fixed text shadow on all headers so they are all the same and consistent.

--- src/components/Atoms/Text/HomePage/ServicesText/HeaderText.js
+++ src/components/Atoms/Text/HomePage/ServicesText/HeaderText.js
@@ -3,7 +3,7 @@ import "./servicesText.css";
 export default function ServicesText() {
     return (
         <div className="headerContainer">
-            <h1 style={{ color: "#000", textShadow: "3px 4px 6px #403b42" }}>De Novo Beauty</h1>
+            <h1 className="servicesNameHeader">De Novo Beauty</h1>
             <h2 className="headerLarge">Popular Services</h2>
         </div>
     );
