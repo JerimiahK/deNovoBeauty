a5b444f792e1d9a0ce2e6108c5b0aed3004c25df
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-03-20 18:22:00 -0600

    Fixed margin issue with smaller screens on Services page.

--- src/components/Atoms/ServiceCards/PersonalMakeup.js
+++ src/components/Atoms/ServiceCards/PersonalMakeup.js
@@ -1,114 +1,58 @@
 import "./personalMakeup.css";
+import {Carousel} from "react-bootstrap";
 import Pic1 from "../../../Assets/Pictures/PersonalMakeup/Personal-Makeup.avif";
 import Pic2 from "../../../Assets/Pictures/PersonalMakeup/Personal-Makeup2.avif";
 
 export default function PersonalMakeupCard() {
-  return (
-      <div className="bridalMakeupContainer">
-          <h3>Bridal Makeup</h3>
-          <div className="bridalCarouselContainer">
-              <Carousel className="servicesCarousel">
-                  {/* Item 1 */}
-                  <Carousel.Item>
-                      <img
-                          className="d-block w-100 bridalImg"
-                          src={Pic1}
-                          alt="First slide"
-                      />
-                  </Carousel.Item>
-                  {/* Item 2 */}
-                  <Carousel.Item>
-                      <img
-                          className="d-block w-100 bridalImg"
-                          src={Pic2}
-                          alt="Second slide"
-                      />
-                  </Carousel.Item>
-              </Carousel>
-          </div>
-          <div>
-              <h3>Bridal Makeup Information</h3>
-              <h6>
-                  <u>
-                      <strong>$50 Deposit To Hold Wedding Day</strong>
-                  </u>
-              </h6>
-              <h6>
-                  <u>
-                      <strong>$50 Travel Fee Depending On Location</strong>
-                  </u>
-              </h6>
-              {/* Bride Options */}
-              <div className="optionsContainer">
-                  <h5>Bride Options</h5>
-                  <ul className="bridalListContainer">
-                      <li>
-                          <strong>$180 -</strong> Bride <strong>ONLY</strong>,
-                          Lash and Makeup Application - 1.5hr
-                      </li>
-                      <li>
-                          <strong>$85 -</strong> Bridal Trial - 1.5hr
-                      </li>
-                  </ul>
-                  <p>
-                      Total For Bride Only: <strong>$265</strong>
-                  </p>
-              </div>
-              {/* Mother Of Bride Options */}
-              <div className="optionsContainer">
-                  <h5>Mother Of Bride Options</h5>
-                  <ul className="bridalListContainer">
-                      <li>
-                          <strong>$85 -</strong> Makeup Application - 1.5hr{" "}
-                          <i>*does not include lashes*</i>
-                      </li>
-                  </ul>
-                  <p>
-                      Total For Mother of Bride: <strong>$85</strong>
-                  </p>
-              </div>
-              {/* Bridesmaid Options */}
-              <div className="optionsContainer">
-                  <h5>Bridesmaid Options</h5>
-                  <p>
-                      <i>*up to 3 bridesmaids*</i>
-                  </p>
-                  <ul className="bridalListContainer">
-                      <li>
-                          <strong>$110/per person -</strong> Lash and Makeup
-                          Application - 45 mins
-                      </li>
-                  </ul>
-                  <p>
-                      Total For 2 Bridesmaids: <strong>$220</strong>
-                  </p>
-                  <p>
-                      Total For 3 Bridesmaids: <strong>$330</strong>
-                  </p>
-              </div>
-              {/* Bridal Package */}
-              <div className="optionsContainer">
-                  <h5>Bridal Package</h5>
-                  <p>
-                      Includes Bride, Bride Trial, MOB, 4 Bridesmaids,
-                      Customized Facial
-                  </p>
-                  <ul className="bridalListContainer">
-                      <li>Bride</li>
-                      <li>Bride Trial - 3hr</li>
-                      <li>
-                          Mother of Bride - 1hr <i>*includes lashes*</i>
-                      </li>
-                      <li>
-                          Bridesmaids - 3hrs <i>*includes lashes*</i>
-                      </li>
-                      <li>Customized Facial</li>
-                  </ul>
-                  <p>
-                      Total For Bridal Package: <strong>$850</strong>
-                  </p>
-              </div>
-          </div>
-      </div>
-  );
+    return (
+        <div className="personalMakeupContainer">
+            <h3>Personal Makeup</h3>
+            <div className="personalCarouselContainer">
+                <Carousel className="servicesCarousel">
+                    {/* Item 1 */}
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100 personalImg"
+                            src={Pic1}
+                            alt="First slide"
+                        />
+                    </Carousel.Item>
+                    {/* Item 2 */}
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100 personalImg"
+                            src={Pic2}
+                            alt="Second slide"
+                        />
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+            <div>
+                <h3>Personal Makeup Information</h3>
+                {/* Basic Application */}
+                <div className="optionsContainer">
+                    <h5>Basic Application</h5>
+                    <ul className="personalListContainer">
+                        <li>
+                            <strong>$85 -</strong> Basic Makeup application -
+                            1hr <i>*lashes not included*</i>
+                        </li>
+                    </ul>
+                    <p>
+                        Total For Bride Only: <strong>$265</strong>
+                    </p>
+                </div>
+                {/* GLAM Application */}
+                <div className="optionsContainer">
+                    <h5>GLAM Applications</h5>
+                    <ul className="personalListContainer">
+                        <li>
+                            <strong>$125 -</strong> GLAM application - 1.5hr{" "}
+                            <i>*lashes included</i>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/components/Molecules/ServicesSection/ServicesContainer.js
+++ src/components/Molecules/ServicesSection/ServicesContainer.js
@@ -1,9 +1,15 @@
 import "./servicesContainer.css";
 import BridalMakeupCard from "../../Atoms/ServiceCards/BridalMakeup";
+import PersonalMakeupCard from "../../Atoms/ServiceCards/PersonalMakeup";
 
 export default function ServicesSection() {
-    return <div className="servicesSectionContainer">
-        <h1 className="servicesHeaderText">Services</h1>
-        <BridalMakeupCard />
-    </div>;
+    return (
+        <div className="servicesSectionContainer">
+            <h1 className="servicesHeaderText">Services</h1>
+            <div className="servicesCardContainer">
+                <BridalMakeupCard />
+                <PersonalMakeupCard />
+            </div>
+        </div>
+    );
 }
