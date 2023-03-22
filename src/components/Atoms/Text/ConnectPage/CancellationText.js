c23be6faf78a5e72c0a26aca77710ad5990d35dd
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-03-21 20:38:06 -0600

    Restyled landing page to clients liking. Increased FAQ's headers to read easier.

--- src/components/Atoms/Text/ConnectPage/CancellationText.js
+++ src/components/Atoms/Text/ConnectPage/CancellationText.js
@@ -3,12 +3,17 @@ import "./cancellationText.css";
 export default function CancellationText() {
     return (
         <div className="cancellationTextContainer">
-            <h5>What Are The Cancellation Fees?</h5>
+            <h4>
+                <strong>What Are The Cancellation Fees?</strong>
+            </h4>
             <p>
                 The cancellation fee comes from the deposit. Deposits are
                 <strong>
                     <i> non-refundable</i>
-                </strong> if the service is canceled 24 hours or less before the scheduled appointment. However, any appointment canceled with more than a 24 hour heads-up is eligible for a deposit refund.
+                </strong>{" "}
+                if the service is canceled 24 hours or less before the scheduled
+                appointment. However, any appointment canceled with more than a
+                24 hour heads-up is eligible for a deposit refund.
             </p>
         </div>
     );
--- src/components/Atoms/Text/ConnectPage/InsuranceText.js
+++ src/components/Atoms/Text/ConnectPage/InsuranceText.js
@@ -3,9 +3,13 @@ import "./insuranceText.css";
 export default function InsuranceText() {
     return (
         <div className="insuranceTextContainer">
-            <h5>Are You Insured?</h5>
+            <h4>
+                <strong>Are You Insured?</strong>
+            </h4>
             <p>
-                I am fully insured by Hands On Trade Association - Certificate #2842150616. This insurance covers Esthetics and everything that is underneath that license.
+                I am fully insured by Hands On Trade Association - Certificate
+                #2842150616. This insurance covers Esthetics and everything that
+                is underneath that license.
             </p>
         </div>
     );
